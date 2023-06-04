75ba2da8604c09f74197bfd34227b30062a2b6d9
atian7064 <56783429+atian7064@users.noreply.github.com>
2023-06-04 12:05:57 -0700

    almost done...

---
+++ js/handle.js
@@ -0,0 +1,5 @@
+//var jsonData;
+//fetch('../database/assignments.json')
+//    .then((response) => response.json())
+//    .then((json) => console.log(json));
+window.location.href = "../list_of_students.html";
